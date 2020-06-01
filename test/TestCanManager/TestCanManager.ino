9f6ddbeb141572594edf8508aabfcfe487340acb
marlinarnz <marlin-arnz@web.de>
2020-06-01 09:44:54 +0200

    completed testing of CanManager

--- test/TestCanManager/TestCanManager.ino
+++ test/TestCanManager/TestCanManager.ino
@@ -144,3 +144,74 @@ test(readSignal_MCU2_MotorTemp_returnOffset) {
   assertEqual(classUnderTest.readSignal(MCU2, MCU2_MotorTemp_LSB, MCU2_MotorTemp_LEN, 1, MCU2_MotorTemp_OFFSET),
               offset);
 }
+
+test(readSignal_MCU2_WarningLevel_success) {
+  float result = 3;
+  classUnderTest.messagesOther[1].writeByte(int(MCU2_WarningLevel_LSB/8), 255);
+  assertEqual(classUnderTest.readSignal(MCU2, MCU2_WarningLevel_LSB, MCU2_WarningLevel_LEN),
+              result);
+  classUnderTest.messagesOther[1].setFrameBytes(0);
+}
+
+test(readSignal_UI_DriveSettings_success) {
+  float result = 43690;
+  classUnderTest.messagesOther[4].setFrameBytes(170);
+  assertEqual(classUnderTest.readSignal(UI_DriveSettings, 18, 17),
+              result);
+  classUnderTest.messagesOther[4].setFrameBytes(0);
+}
+
+test(readSignal_MCU1_ActMotorTq_success) {
+  float result = 85;
+  classUnderTest.messagesOther[0].writeByte(MCU1_ActMotorTq_LSB/8, int(result), MCU1_ActMotorTq_CONV_D);
+  assertNear(classUnderTest.readSignal(MCU1, MCU1_ActMotorTq_LSB, MCU1_ActMotorTq_LEN, MCU1_ActMotorTq_CONV_D),
+             result, 0.8);
+  classUnderTest.messagesOther[0].setFrameBytes(0);
+}
+
+test(readSignal_MCU1_ActMotorSpd_success) {
+  float result = 10922.5;
+  classUnderTest.messagesOther[0].setFrameBytes(170);
+  assertEqual(classUnderTest.readSignal(MCU1, MCU1_ActMotorSpd_LSB, MCU1_ActMotorSpd_LEN, MCU1_ActMotorSpd_CONV_D),
+              result);
+  classUnderTest.messagesOther[0].setFrameBytes(0);
+}
+
+test(readSignal_MCU2_MotorTemp_success) {
+  float result = 35;
+  classUnderTest.messagesOther[1].writeByte(MCU2_MotorTemp_LSB/8, int(result), 1, MCU2_MotorTemp_OFFSET);
+  assertEqual(classUnderTest.readSignal(MCU2, MCU2_MotorTemp_LSB, MCU2_MotorTemp_LEN, 1, MCU2_MotorTemp_OFFSET),
+              result);
+  classUnderTest.messagesOther[1].setFrameBytes(0);
+}
+
+test(writeSignal_allMessagesOneByte_success) {
+  for (uint8_t i=0; i<N_VCU_MESSAGES; i++) {
+    for (uint8_t lsb=8; lsb<8*8; lsb++) {
+      classUnderTest.writeSignal(classUnderTest.messagesVCU[i].getId(), lsb, 8, 170);
+      float result = 170;
+      assertEqual(classUnderTest.messagesVCU[i].readSignalBE(lsb, 8),
+                  result);
+    }
+    classUnderTest.messagesVCU[i].setFrameBytes(0);
+  }
+}
+
+test(writeSignal_conv_success) {
+  float result = 777;
+  float conv = 0.7;
+  classUnderTest.writeSignal(VCU1, 47, 17, int(result), conv);
+  assertNear(classUnderTest.messagesVCU[0].readSignalBE(47, 17, conv),
+             result, 0.77);
+  classUnderTest.messagesVCU[0].setFrameBytes(0);
+}
+
+test(writeSignal_offset_success) {
+  float result = 77;
+  float conv = 1;
+  int offset = 7;
+  classUnderTest.writeSignal(VCU1, 12, 7, int(result), conv, offset);
+  assertNear(classUnderTest.messagesVCU[0].readSignalBE(12, 7, conv, offset),
+             result, 0.77);
+  classUnderTest.messagesVCU[0].setFrameBytes(0);
+}
